b31e64e43739cc8ed7961bdcffd698ed793e91d1
Olga Nadrshina <nadolga.nl@gmail.com>
2022-09-23 16:54:37 +0200

    First API project based on classes

---
+++ src/components/randomChar/RandomChar.js
@@ -0,0 +1,114 @@
+import { Component } from 'react';
+import MarvelService from '../../services/MarvelService';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
+
+import './randomChar.scss';
+import mjolnir from '../../resources/img/mjolnir.png';
+
+class RandomChar extends Component {
+    constructor(props) {
+        super(props);        
+    }
+
+    state = {
+        char: {},
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();        
+    }
+
+    componentWillUnmount() {        
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char,
+            loading: false,
+            error: false
+        })
+    }
+    onCharloading = () => {
+        this.setState({
+            loading: true
+
+        })
+    }
+    
+    onError = (char) => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    updateChar = () => {
+        this.onCharloading();
+        const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+        this.marvelService
+            .getCharacter(id)
+            .then(this.onCharLoaded)
+            .catch(this.onError)
+    }
+
+    render() {
+        const {char, loading, error} = this.state;
+        const spinner = loading ? <Spinner/> : null;
+        const errorMessage = error ? <ErrorMessage/> : null;
+        const content = !(error || loading) ? <View char={char}/> : null;
+        return (
+            <div className="randomchar">
+                {spinner}
+                {errorMessage}
+                {content}
+                    <div className="randomchar__static">
+                    <p className="randomchar__title">
+                        Random character for today!<br/>
+                        Do you want to get to know him better?
+                    </p>
+                    <p className="randomchar__title">
+                        Or choose another one
+                    </p>
+                    <button 
+                        className="button button__main"
+                        onClick={this.updateChar}>
+                            <div className="inner">try it</div>
+                    </button>
+                    <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+                </div>
+            </div>
+        )
+    }
+}
+const View = ({char}) => {
+    const {name, description, thumbnail, homepage, wiki} = char;
+    let imgStyle = {"object-fit": 'cover'};
+    if (thumbnail == 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+        imgStyle = {"object-fit": 'contain'}
+    }
+    return (
+        <div className="randomchar__block">
+            <img src={thumbnail} alt="Random character" className="randomchar__img" style={imgStyle}/>
+            <div className="randomchar__info">
+                <p className="randomchar__name">{name}</p>
+                <p className="randomchar__descr">
+                    {description}
+                </p>
+                <div className="randomchar__btns">
+                    <a href={homepage} className="button button__main">
+                        <div className="inner">homepage</div>
+                    </a>
+                    <a href={wiki} className="button button__secondary">
+                        <div className="inner">Wiki</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default RandomChar;
